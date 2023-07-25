67007501a9ceb0910eb98987723147999dff9096
MaheshMERN <88592368+MaheshMERN@users.noreply.github.com>
2023-07-25 23:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+ const x = window.innerWidth;
+      const y = window.innerHeight;
+document.getElementById("sizeInfo").innnerHTML = `<h1>Width: ${x} and Height: ${y}</h1>`
